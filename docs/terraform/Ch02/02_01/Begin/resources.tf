b133c63442eb38e7d2fde58d91c608dff4c31cdb
Michael Sawyers <msawyers@iii.com>
2019-09-16 08:18:00 -0700

    Comment out non-aws resources

--- docs/terraform/Ch02/02_01/Begin/resources.tf
+++ docs/terraform/Ch02/02_01/Begin/resources.tf
@@ -1,7 +1,7 @@
-resource "google_compute_network" "our_development_network" {
-  name = "devnetwork"
-  auto_create_subnetworks = False
-}
+# resource "google_compute_network" "our_development_network" {
+#   name = "devnetwork"
+#   auto_create_subnetworks = False
+# }
 
 resource "aws_vpc" "environment-example-two" {
   cidr_block = "10.0.0.0/16"
